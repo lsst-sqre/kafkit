954926dcfe9c01dd7052ddfe364079d6937e21a3
Jonathan Sick <jsick@lsst.org>
2019-01-11 11:06:34 -0700

    Add registry-enabled Serializer + Deserializer
    This adds Serializer and Deserializer classes that can be used by
    producers and consumers to send and recieve messages in the Confluent
    Wire Format (https://docs.confluent.io/current/schema-registry/docs/serializer-formatter.html#wire-format)
    This means that schema ID in the schema registry are included as a
    prefix to the avro-encoded message.
    
    The Serializer is dedicated to working for a specific schema. This way
    is can be used as a value_serializer or key_serializer by the Producer
    (see
    https://aiokafka.readthedocs.io/en/stable/examples/serialize_and_compress.html#serialization-and-compression).
    
    The Deserializer needs to react to whatever schema ID gets send to it.
    For that reason, it can't implement a __call__ (it'd need to be a
    coroutine), and thus must always be used externally to the consumer.

--- kafkit/registry/serializer.py
+++ kafkit/registry/serializer.py
@@ -2,7 +2,206 @@
 Confluent Schema Registry.
 """
 
+__all__ = ('Serializer', 'Deserializer', 'pack_wire_format_prefix',
+           'unpack_wire_format_data')
+
+from io import BytesIO
 import struct
+
+import fastavro
+
+
+class Serializer:
+    """An Avro message serializer that writes in the Confluent Wire Format.
+
+    Use the `Serializer.register` class method to create a Serializer instance.
+
+    Parameters
+    ----------
+    schema : `dict`
+        An Avro schema.
+    schema_id : `int`
+        The ID of ``schema`` in a Schema Registry.
+
+    Notes
+    -----
+    A `Serializer` instance is dedicated to serializing messages of a single
+    schema. The serializer is a callable. Send the message to the serializer
+    to encode it.
+
+    This is an example using the MockRegistryApi client. Real-world
+    applications would use the `kafkit.registry.aiohttp.RegistryApi` instead.
+
+    >>> from kafkit.registry.sansio import MockRegistryApi
+    >>> client = MockRegistryApi()
+
+    For demonstration purposes, assume the schema is already cached:
+
+    >>> schema = {
+    ...    'type': 'record',
+    ...    'name': 'schema1',
+    ...    'namespace': 'test-schemas',
+    ...    'fields': [
+    ...        {'name': 'a', 'type': 'int'},
+    ...        {'name': 'b', 'type': 'string'}
+    ...    ]
+    ... }
+    >>> client.schemas.insert(schema, 1)
+    >>> serializer = await Serializer.register(
+    ...     registry=client,
+    ...     schema=schema)
+
+    Serialize messages:
+
+    >>> data = serializer({'a': 42, 'b': 'Hello world!'})
+
+    This serializer works well as ``key_serializer`` or ``value_serializer``
+    parameters to the `aiokafka.AIOKafkaProducer`. See
+    https://aiokafka.readthedocs.io/en/stable/examples/serialize_and_compress.html
+    """
+
+    def __init__(self, *, schema, schema_id):
+        self.schema = fastavro.parse_schema(schema)
+        self.id = schema_id
+
+    @classmethod
+    async def register(cls, *, registry, schema, subject=None):
+        """Create a serializer ensuring that the schema is registered with the
+        schema registry.
+
+        Parameters
+        ----------
+        registry : `kafkit.registry.sansio.RegistryApi`
+            A registry client.
+        schema : `dict`
+            An Avro schema.
+        subject : `str`, optional
+            The name of the Schema Registry subject that the schema is
+            registered under. If not provided, the schema is automatically
+            set from the fully-qualified name of the schema (the ``'name'``
+            field of the schema's record type). See
+            `kafkit.registry.sansio.RegistryApi.register_schema` for details.
+
+        Returns
+        -------
+        serializer : `Serializer`
+            A serializer instance.
+
+        Notes
+        -----
+        It's safe to call this method even if you know that the schema has been
+        registered before. This process is necessary to get the schema's ID,
+        and won't create a new schema if an identical schema is already
+        registered.
+        """
+        id_ = await registry.register_schema(schema, subject=subject)
+        return cls(schema=schema, schema_id=id_)
+
+    def __call__(self, data):
+        """Serialize a dataset in the Confluent Schema Registry Wire format,
+        which is an Avro-encoded message with a schema-identifying prefix.
+
+        Parameters
+        ----------
+        data : object
+            An Avro-serializable object. The object must conform to the schema.
+
+        Returns
+        -------
+        message : `bytes`
+            Message in the Confluent Schema Registry wire format.
+        """
+        message_fh = BytesIO()
+        # Write the Confluent Wire Format prefix.
+        message_fh.write(pack_wire_format_prefix(self.id))
+        # Write the Avro-encoded message
+        fastavro.schemaless_writer(message_fh, self.schema, data)
+        message_fh.seek(0)
+        return message_fh.read()
+
+
+class Deserializer:
+    """An Avro message deserializer that understands the Confluent Wire Format
+    and obtains schemas on-demand from a Confluent Schema Registry.
+
+    Parameters
+    ----------
+    registry : `kafkit.registry.sansio.RegistryApi`
+        A registry client.
+
+    Notes
+    -----
+    The Deserializer works exclusively with Avro-encoded messages in the
+    `Confluent Wire Format`_. This means that schemas for messages must be
+    available from a `Confluent Schema Registry`_.
+
+    When an encoded message is deserialized in the `~Deserializer.deserialize`
+    method, it does the following steps:
+
+    1. Unpacks the wire format prefix to discover the ID of the
+       message's schema in the schema registry.
+    2. Obtains the schema from the `~kafkit.registry.sansio.RegistryApi`.
+       Schemas are cached, so this is a fast operation.
+    3. Decodes the message using `fastavro.schemaless_reader`.
+
+    **Why not implement a __call__ method?**
+
+    The `Serializer` implements a ``__call__`` method so that it can be used
+    as a key or value serializer by the aiokafka producer. This Deserializer
+    doesn't do that because `Deserializer.deserialize` is a coroutine
+    (internally it works with the asynchronous
+    `~kafkit.registry.sansio.RegistryApi`) and magic methods can't be
+    coroutines. It's not the end of the world, though, just call
+    `~Deserializer.deserialize` manually on by bytes obtained by the consumer.
+
+    .. _`Confluent Wire Format`:
+       https://docs.confluent.io/current/schema-registry/docs/serializer-formatter.html#wire-format
+    .. _`Confluent Schema Registry`:
+       https://docs.confluent.io/current/schema-registry/docs/index.html
+    """
+
+    def __init__(self, *, registry):
+        self._registry = registry
+
+    async def deserialize(self, data, include_schema=False):
+        """Deserialize a message.
+
+        Parameters
+        ----------
+        data : `bytes`
+            The encoded message, usually obtained directly from a Kafka
+            consumer. The message must be in the Confluent Wire Format.
+        include_schema : `bool`, optional
+            If `True`, the schema itself is included in the returned value.
+            This is useful if your application operates on many different
+            types of messages, and needs a convenient way to introspect
+            a message's type.
+
+        Returns
+        -------
+        message_info : `dict`
+            The deserialized message is wrapped in a dictionary to include
+            metadata. The keys are:
+
+            ``'id'``
+                The ID of the schema (an `int`) in the Schema Registry. This
+                uniquely identifies the message's schema.
+            ``'message'``
+                The message itself, as a decoded Python object.
+            ``'schema'``
+                The schema, as a Python object. This key is only included
+                when ``include_schema`` is `True`.
+        """
+        schema_id, message_data = unpack_wire_format_data(data)
+        schema = await self._registry.get_schema_by_id(schema_id)
+
+        message_fh = BytesIO(message_data)
+        message_fh.seek(0)
+        message = fastavro.schemaless_reader(message_fh, schema)
+        result = {'id': schema_id, 'message': message}
+        if include_schema:
+            result['schema'] = schema
+        return result
 
 
 def pack_wire_format_prefix(schema_id):
--- tests/test_registry_serializer.py
+++ tests/test_registry_serializer.py
@@ -1,10 +1,15 @@
 """Tests for the kafkit.registry.serializer module.
 """
 
+import json
+from io import BytesIO
+
+import fastavro
 import pytest
 
 from kafkit.registry.serializer import (
-    pack_wire_format_prefix, unpack_wire_format_data)
+    pack_wire_format_prefix, unpack_wire_format_data, Serializer, Deserializer)
+from kafkit.registry.sansio import MockRegistryApi
 
 
 def test_wire_format():
@@ -25,3 +30,95 @@ def test_wire_format():
 def test_unpacking_short_message():
     with pytest.raises(RuntimeError):
         unpack_wire_format_data(b'')
+
+
+@pytest.mark.asyncio
+async def test_serializer():
+    """Test the Serializer class.
+    """
+    client = MockRegistryApi(
+        body=json.dumps({'id': 1}).encode('utf-8')
+    )
+    schema1 = {
+        'type': 'record',
+        'name': 'schema1',
+        'namespace': 'test-schemas',
+        'fields': [
+            {'name': 'a', 'type': 'int'},
+            {'name': 'b', 'type': 'string'}
+        ]
+    }
+    serializer = await Serializer.register(registry=client, schema=schema1)
+    assert serializer.id == 1
+
+    message = {'a': 1, 'b': 'helloworld'}
+    data = serializer(message)
+    assert isinstance(data, bytes)
+
+    # Check that the message can be deserialized
+    # First, the wire format prefix
+    unpacked_id, unpacked_body = unpack_wire_format_data(data)
+    assert unpacked_id == serializer.id
+    # Second, the message
+    unpacked_schema = client.schemas[unpacked_id]
+    message_fh = BytesIO(unpacked_body)
+    message_fh.seek(0)
+    unpacked_message = fastavro.schemaless_reader(message_fh, unpacked_schema)
+    assert unpacked_message == message
+
+
+@pytest.mark.asyncio
+async def test_deserializer():
+    """Test the Deserializer class.
+    """
+    # First schema
+    schema1 = {
+        'type': 'record',
+        'name': 'schema1',
+        'namespace': 'test-schemas',
+        'fields': [
+            {'name': 'a', 'type': 'int'},
+            {'name': 'b', 'type': 'string'}
+        ]
+    }
+    schema1_id = 1
+    # Second schema
+    schema2 = {
+        'type': 'record',
+        'name': 'schema2',
+        'namespace': 'test-schemas',
+        'fields': [
+            {'name': 'c', 'type': 'int'},
+            {'name': 'd', 'type': 'string'}
+        ]
+    }
+    schema2_id = 2
+    # insert the schemas directly into the cache
+    client = MockRegistryApi()
+    client.schemas.insert(schema1, schema1_id)
+    client.schemas.insert(schema2, schema2_id)
+
+    # Serialize a couple messages, using the serializer. Since the schema
+    # is cached it won't need a mocked response body.
+    serializer1 = await Serializer.register(registry=client, schema=schema1)
+    serializer2 = await Serializer.register(registry=client, schema=schema2)
+
+    message_1 = {'a': 42, 'b': 'hello'}
+    data_1 = serializer1(message_1)
+
+    message_2 = {'c': 13, 'd': 'bonjour'}
+    data_2 = serializer2(message_2)
+
+    # Deserialization
+    deserializer = Deserializer(registry=client)
+
+    response_1 = await deserializer.deserialize(data_1)
+    assert response_1['id'] == schema1_id
+    assert response_1['message'] == message_1
+    assert 'schema' not in response_1
+
+    response_2 = await deserializer.deserialize(data_2, include_schema=True)
+    assert response_2['id'] == schema2_id
+    assert response_2['message'] == message_2
+    assert 'schema' in response_2
+    assert response_2['schema']['name'] == 'test-schemas.schema2'
