b94530ee8fde523a7e79ebdf47a1d2cf078e9a91
Jonathan Sick <jsick@lsst.org>
2019-02-13 13:10:56 -0700

    Return data from put/patch/post/delete methods
    This was a bad oversight!

--- kafkit/registry/sansio.py
+++ kafkit/registry/sansio.py
@@ -252,6 +252,7 @@ class RegistryApi(metaclass=abc.ABCMeta):
             wrong with the server itself.
         """
         data = await self._make_request("PATCH", url, url_vars, data)
+        return data
 
     async def put(self, url, url_vars=dict(), data=b""):
         """Send an HTTP PUT request.
@@ -286,6 +287,7 @@ class RegistryApi(metaclass=abc.ABCMeta):
             wrong with the server itself.
         """
         data = await self._make_request("PUT", url, url_vars, data)
+        return data
 
     async def delete(self, url, url_vars=dict(), *, data=b""):
         """Send an HTTP DELETE request.
@@ -320,6 +322,7 @@ class RegistryApi(metaclass=abc.ABCMeta):
             wrong with the server itself.
         """
         data = await self._make_request("DELETE", url, url_vars, data)
+        return data
 
     @staticmethod
     def _prep_schema(schema):
